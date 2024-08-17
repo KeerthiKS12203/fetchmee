975a70ff232fa53ffea145b70ebb2789ea487eae
KeerthiKS12203 <keerthiks1222003@gmail.com>
2024-08-17 10:52:21 +0530

    Added scanner within scan screen, barcode scanner enabled, manufacture month/year format modified

--- app/src/main/java/com/example/qrcodescanner2/fragments/ScanFragment.java
+++ app/src/main/java/com/example/qrcodescanner2/fragments/ScanFragment.java
@@ -18,6 +18,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,15 +31,23 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -51,14 +60,14 @@ import okhttp3.Response;
 
 public class ScanFragment extends Fragment {
 
-    FusedLocationProviderClient fusedLocationProviderClient;
-    Button scan_btn;
-//    Button manual_page_nav;
-    TextView text_api_str, text_str, text_loc, text_android_id, text_api_call_status, api_call_status_label, api_call_status_colon;
-    String str, str_loc, loc;
-    boolean locObtained = false, strObtained = false;
-    int i = 0;
-
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Button scanBtn;
+    private TextView textStr, textLoc, textAndroidId, textApiCallStatus, apiCallStatusLabel, apiCallStatusColon;
+    private String str, loc;
+    private boolean locObtained = false, strObtained = false;
+    private FrameLayout scannerContainer;
+    private CompoundBarcodeView barcodeView;
     private static String SERVER_URL;
 
     public ScanFragment() {
@@ -76,202 +85,204 @@ public class ScanFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
 
-        scan_btn = view.findViewById(R.id.scanner);
-//        manual_page_nav = view.findViewById(R.id.manual_page_nav);
-        text_api_str = view.findViewById(R.id.text_api_str);
-        text_loc = view.findViewById(R.id.text_loc);
-        text_str = view.findViewById(R.id.text_str);
-        text_android_id = view.findViewById(R.id.text_android_id);
-        text_api_call_status =  view.findViewById(R.id.text_api_call_status);
-        api_call_status_label = view.findViewById(R.id.api_call_status_label);
-        api_call_status_colon = view.findViewById(R.id.api_call_status_colon);
-        text_loc.setText("Location");
-        text_str.setText("QR String");
-        text_api_str.setText("API call:Not Made");
-        text_android_id.setText("android id not obtained");
-        text_api_call_status.setText("API call: pending");
+        scanBtn = view.findViewById(R.id.scanner);
+        textLoc = view.findViewById(R.id.text_loc);
+        textStr = view.findViewById(R.id.text_str);
+        textAndroidId = view.findViewById(R.id.text_android_id);
+        textApiCallStatus = view.findViewById(R.id.text_api_call_status);
+        apiCallStatusLabel = view.findViewById(R.id.api_call_status_label);
+        apiCallStatusColon = view.findViewById(R.id.api_call_status_colon);
+        scannerContainer = view.findViewById(R.id.scanner_container);
+
+        textLoc.setText("Location");
+        textStr.setText("QR String");
+        textAndroidId.setText("Android ID not obtained");
+        textApiCallStatus.setText("API call: pending");
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
         GlobalVariables globalVariables = GlobalVariables.getInstance();
-        text_android_id.setText(globalVariables.getAndroidId());
-
         String androidId = Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ANDROID_ID);
         globalVariables.setAndroidId(androidId);
-        text_android_id.setText(globalVariables.getAndroidId());
-        //concatenate android id to server url SERVER_URL=http://devmdas.fetchmee.in:15000/androidId/info/gps/1
+        textAndroidId.setText(globalVariables.getAndroidId());
+
         SERVER_URL = globalVariables.getSERVER_URL();
-        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-            }, 100);
-        }
+        checkLocationPermission();
 
-        scan_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IntentIntegrator intentIntegrator=
-                        IntentIntegrator.forSupportFragment(ScanFragment.this);
-                intentIntegrator.setPrompt("Scan a QR Code");
-                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                intentIntegrator.setOrientationLocked(true);
-                intentIntegrator.initiateScan();
+        setupBarcodeView();
+        startScan();
 
-
+        scanBtn.setOnClickListener(v -> {
+            try {
+                if (strObtained && locObtained) {
+                    sendScannedDataToServer(globalVariables.getMeterRatings(), String.valueOf(globalVariables.getCurrentLatitude()), String.valueOf(globalVariables.getCurrentLongitude()));
+                } else {
+                    Toast.makeText(getActivity(), "Data not complete. Ensure QR code is scanned and location is obtained.", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
             }
         });
-
-//        manual_page_nav.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                ManualFragment manualFragment = new ManualFragment();
-////
-////                // Get the FragmentManager
-////                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-////
-////                // Begin a transaction to replace the current fragment with the new fragment
-////                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-////                fragmentTransaction.replace(R.id.view_pager, manualFragment);
-////                fragmentTransaction.addToBackStack(null);
-////                fragmentTransaction.commit();
-//////////
-//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-//                transaction.replace(R.id.scan_frame, manualFragment ); // give your fragment container id in first parameter
-//                transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
-//                transaction.commit();
-////////
-//
-//
-//                getActivity().getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.tab_layout, manualFragment, "findThisFragment")
-//                        .addToBackStack(null)
-//                        .commit();
-//            }
-//        });
 
         return view;
     }
 
+    private void setupBarcodeView() {
+        barcodeView = new CompoundBarcodeView(getContext());
+        barcodeView.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+        ));
+        scannerContainer.addView(barcodeView);
+
+        List<BarcodeFormat> formats = Arrays.asList(
+                BarcodeFormat.UPC_A,
+                BarcodeFormat.UPC_E,
+                BarcodeFormat.EAN_8,
+                BarcodeFormat.EAN_13,
+                BarcodeFormat.CODABAR,
+                BarcodeFormat.CODE_39,
+                BarcodeFormat.CODE_93,
+                BarcodeFormat.CODE_128,
+                BarcodeFormat.ITF,
+                BarcodeFormat.RSS_14,
+                BarcodeFormat.RSS_EXPANDED,
+                BarcodeFormat.QR_CODE,
+                BarcodeFormat.DATA_MATRIX,
+                BarcodeFormat.PDF_417,
+                BarcodeFormat.AZTEC,
+                BarcodeFormat.MAXICODE,
+                BarcodeFormat.UPC_EAN_EXTENSION
+
+        );
+
+        // Create a barcode decoder to handle various barcode formats
+        barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+
+        barcodeView.decodeContinuous(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                str = result.getText();
+                if (str != null) {
+                    textStr.setText(str);
+                    GlobalVariables globalVariables = GlobalVariables.getInstance();
+                    globalVariables.setMeterRatings(str);
+                    strObtained = true;
+                    getLastLocation();
+                    barcodeView.pause(); // Pause scanning to prevent rapid re-scans
+                    barcodeView.resume(); // Resume scanning after processing
+                } else {
+                    Log.d("ScanFragment", "String is null");
+                }
+            }
+
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
+                // Handle possible result points if needed
+            }
+        });
+    }
+
+    private void startScan() {
+        scannerContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (intentResult != null) {
-            str = intentResult.getContents();
-            i = 0;
-            if (str != null) {
-                text_str.setText(str);
-                GlobalVariables globalVariables = GlobalVariables.getInstance();
-                globalVariables.setMeterRatings(str);
-                strObtained = true;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
                 getLastLocation();
             } else {
-                System.out.println("String is null");
-                super.onActivityResult(requestCode, resultCode, data);
+                Toast.makeText(getActivity(), "Location permission denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     private void getLastLocation() {
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-            // Request high accuracy location
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             Task<Location> locationTask = fusedLocationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, null);
 
-            // Add a callback to handle the location result
-            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    // Handle the location result
-                    if(location!=null){
-                        loc="Latitude    : "+location.getLatitude()+"\nLongitude : "+location.getLongitude();
-                        GlobalVariables globalVariables=GlobalVariables.getInstance();
-                        globalVariables.setCurrentLatitude(location.getLatitude());
-                        globalVariables.setCurrentLongitude(location.getLongitude());
-                        globalVariables.setCurrentLocation(loc);
-                        text_loc.setText(globalVariables.getCurrentLocation());
-                        try {
-                            sendScannedDataToServer(globalVariables.getMeterRatings(), String.valueOf(location.getLatitude()),String.valueOf(location.getLongitude()));
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
+            locationTask.addOnSuccessListener(location -> {
+                if (location != null) {
+                    loc = "Latitude: " + location.getLatitude() + "\nLongitude: " + location.getLongitude();
+                    GlobalVariables globalVariables = GlobalVariables.getInstance();
+                    globalVariables.setCurrentLatitude(location.getLatitude());
+                    globalVariables.setCurrentLongitude(location.getLongitude());
+                    globalVariables.setCurrentLocation(loc);
+                    textLoc.setText(globalVariables.getCurrentLocation());
+                    locObtained = true;
+                    scanBtn.setVisibility(View.VISIBLE);
                 }
-
-            });
-
-            // Add a callback to handle any errors
-            locationTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    // Handle the error
-                    Log.e("Location", "Error getting location", e);
-                }
-            });
-        }else{
-
+            }).addOnFailureListener(e -> Log.e("Location", "Error getting location", e));
         }
     }
 
     private void sendScannedDataToServer(String scannedData, String lat, String lng) throws JSONException {
         OkHttpClient client = new OkHttpClient();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyy HHmmss");
-        // Format the current date and time using the SimpleDateFormat
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         String scanTimeDate = dateFormat.format(new Date());
-        String mode="S";
-        GlobalVariables globalVariables=GlobalVariables.getInstance();
+        System.out.println("scanTimeDate: "+scanTimeDate);
+        String mode = "S";
+        GlobalVariables globalVariables = GlobalVariables.getInstance();
 
-        DataObject wrapper = new DataObject(scannedData, lat,lng, mode,scanTimeDate);
+        DataObject wrapper = new DataObject(scannedData, lat, lng, mode, scanTimeDate);
         JSONObject jsonObject = wrapper.createJsonObject();
         RequestBody requestBody = RequestBody.create(jsonObject.toString(), MediaType.get("application/json"));
-        text_api_str.setText(jsonObject.toString());
 
         Request request = new Request.Builder()
                 .url(globalVariables.getSERVER_URL())
                 .post(requestBody)
                 .build();
 
-        api_call_status_label.setVisibility(View.VISIBLE);
-        api_call_status_colon.setVisibility(View.VISIBLE);
-        text_api_call_status.setVisibility(View.VISIBLE);
-
-
+        apiCallStatusLabel.setVisibility(View.VISIBLE);
+        apiCallStatusColon.setVisibility(View.VISIBLE);
+        textApiCallStatus.setVisibility(View.VISIBLE);
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getActivity(),  "Request failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        text_api_call_status.setText("Request failed: " + e.getMessage());
-                    }
+                getActivity().runOnUiThread(() -> {
+                    Toast.makeText(getActivity(), "Request failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    textApiCallStatus.setText("Request failed: " + e.getMessage());
                 });
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseBody = response.body().string();
-                if (response.isSuccessful()) {
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            System.out.println("Server response: " + responseBody);
-                            Toast.makeText(getActivity(), "Request successful. Response code: " + response.code() + " - " + responseBody, Toast.LENGTH_SHORT).show();
-                            text_api_call_status.setText(response.code()+", Request successful.");
-                        }
-                    });
-                } else {
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            System.out.println("Server response: " + responseBody);
-                            Toast.makeText(getActivity(), "Request unsuccessful. Response code: " + response.code() + " - " + responseBody, Toast.LENGTH_SHORT).show();
-                            text_api_call_status.setText(response.code()+", Request unsuccessful." );
-                        }
-                    });
-                }
+                getActivity().runOnUiThread(() -> {
+                    if (response.isSuccessful()) {
+                        Toast.makeText(getActivity(), "Request successful. Response code: " + response.code() + " - " + responseBody, Toast.LENGTH_SHORT).show();
+                        textApiCallStatus.setText(response.code() + ", Request successful.");
+                    } else {
+                        Toast.makeText(getActivity(), "Request unsuccessful. Response code: " + response.code() + " - " + responseBody, Toast.LENGTH_SHORT).show();
+                        textApiCallStatus.setText(response.code() + ", Request unsuccessful.");
+                    }
+                });
             }
         });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        barcodeView.pause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        barcodeView.resume();
     }
 }
